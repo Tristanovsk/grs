1bc252ebd135a00956d0e14d94a77465feff26b1
Harmel Tristan <tristan.harmel@get.omp.eu>
2019-01-23 18:22:54 +0100

    test gitignore for config.py

--- grs/config.py
+++ grs/config.py
@@ -14,7 +14,7 @@ NDWI_threshold = [0., 1.1]
 
 # -----------------
 # please set the following path according to your specific file tree
-#grs_root = os.path.abspath('/DATA/S2_processing/PYTHON/grs/grs')
+# grs_root = os.path.abspath('/DATA/S2_processing/PYTHON/grs/grs')
 grs_root = root
 
 # data_root = os.path.abspath('/DATA/S2_processing/PYTHON/grs/grs/../..')
