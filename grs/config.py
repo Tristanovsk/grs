f1e6de7e3d93a37efdef67dfc9a7c2d855ad442c
Tristan Harmel <tristan.harmel@get.omp.eu>
2019-02-11 11:29:18 +0100

    conflict with remote and local config

---
+++ grs/config.py
@@ -0,0 +1,32 @@
+''' Set absolute path '''
+
+import os, sys
+from .__init__ import __version__ as VERSION
+from .__init__ import __package__
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+
+# -----------------
+# set values bracketing the Normalized Difference Water Index for rough land/water masking
+NDWI_threshold = [0., 1.1]
+
+
+# -----------------
+# please set the following path according to your specific file tree
+# grs_root = os.path.abspath('/DATA/S2_processing/PYTHON/grs/grs')
+grs_root = root
+
+# data_root = os.path.abspath('/DATA/S2_processing/PYTHON/grs/grs/../..')
+data_root = os.path.abspath('/DATA/git/satellite_app/grsdata')
+#directory to store temporary unzipped files
+tmp_dir = os.path.abspath('/tmp')
+lut_root = os.path.join(data_root, "LUT")
+cams_folder = os.path.abspath('/DATA/ECMWF/CAMS') #os.path.join(data_root, "CAMS")
+smac_root = os.path.join(data_root, "SMAC_COEFS")
+
+
+# -----------------
+# do not change:
+fortran_root = os.path.join(root, "fortran")
+sys.path.insert(0, fortran_root)
