5e43fa5020b6ce14b822c2f7197c6df7586a5f58
Betmont Cecile <cecile.betmont@thalesgroup.com>
2022-03-15 12:59:15 +0000

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     if(len(sys.argv)>1):
         config_file=sys.argv[1]
     else:
-        config_file="/app/grs/grs/config.yml"
+        config_file="/app/grs/grs/global_config.yml"
 
     with open(config_file, 'r') as yamlfile:
         data = yaml.load(yamlfile, Loader=yaml.FullLoader)
@@ -17,27 +17,36 @@ if __name__ == '__main__':
     os.environ['DATA_ROOT'] = data['data_root']
     os.environ['CAMS_PATH'] = data['cams_folder']
 
-
     for key, value in data.items():
         if(value is not None and value!=''):
             data[key]=value 
             else:
             data[key]=None
-            #return
-            try:
-                from grs import grs_process
-                grs_process.process().execute(file_tbpd=data["file_tbpd"], outfile=data["outfile"], wkt=data["wkt"], 
-                altitude=data["altitude"], aerosol=data["aerosol"],
-                dem=data["dem"], aeronet_file=data["aeronet_file"], resolution=data["resolution"],
-                aot550=data["aot550"], angstrom=data["angstrom"], unzip=data["unzip"], 
-                untar=data["untar"], startrow=data["startrow"], angleonly=data["angleonly"])
-            except:
-                print('-------------------------------')
-                print('error for file  ', file_tbp, ' skip')
-                print('-------------------------------')
-                with open(config["logfile"], "a") as myfile:
-                    myfile.write(file_tbp + ' error during grs \n')
-                continue
-        # here sys.exit instead of "return" to terminate and close snappy and free memory
-        #sys.exit()
-        return
+            
+    lonmin, lonmax = -180, 180
+    latmin, latmax = -90,90#-21.13
+    wkt_rect = "POLYGON((" + str(lonmax) + " " + str(latmax) + "," + str(lonmax) + " " + str(latmin) + "," + str(lonmin) + " " + str(latmin) + "," + str(lonmin) + " " + str(latmax) + "," + str(lonmax) + " " + str(latmax) + "))"
+
+    if os.path.isfile(outfile + ".dim") & data['noclobber']:
+            print('File ' + outfile + ' already processed; skip!')
+            continue
+        # skip if incomplete (enables multiprocess)
+    if os.path.isfile(outfile + ".dim.incomplete"):# & False:
+        print('found incomplete File ' + outfile + '; skip!')
+        continue
+
+    try:
+        from grs import grs_process
+        grs_process.process().execute(file_tbpd=data["file_tbpd"], outfile=data["outfile"], wkt=wkt_rect, 
+        altitude=data["altitude"], aerosol=data["aerosol"],
+        dem=data["dem"], aeronet_file=data["aeronet_file"], resolution=data["resolution"],
+        aot550=data["aot550"], angstrom=data["angstrom"], unzip=data["unzip"], 
+        untar=data["untar"], startrow=data["startrow"], angleonly=data["angleonly"])
+    except:
+        print('-------------------------------')
+        print('error for file  ', file_tbp, ' skip')
+        print('-------------------------------')
+        with open(config["logfile"], "a") as myfile:
+            myfile.write(file_tbp + ' error during grs \n')
+        continue
+
