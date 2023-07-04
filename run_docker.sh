37c7cdaf807a3bd27679d6b8e85d3e16259abf27
Buratti Robin (MAGELLIUM)-EXT <robin.buratti@magellium.fr>
2023-07-04 08:27:30 +0000

    [run_docker.sh] Update run_docker.sh for GRS 2.0.5
    Former-commit-id: 50721892a4c0ab0f227255f9dd32494247c0ba03

--- run_docker.sh
+++ run_docker.sh
@@ -5,6 +5,7 @@ echo The CAMS files are located at........"$3"
 echo The output file will be named........"$4"
 echo The output file will located at......"$5"
 echo The requested resolution is.........."$6"
+echo The SURFWATER files are located at..."$7"
 
 # Create and parametrize container
 sudo docker run -d --name grs2 $1
@@ -12,7 +13,12 @@ sudo docker cp $2 grs2:/home/L1C
 sudo docker cp $3 grs2:/home/CAMS
 
 # Launch processing and retreive result
-sudo docker exec grs2 grs /home/L1C --cams_file /home/CAMS -o $4 --resolution $6
+if [ -z "$7" ]
+then
+    sudo docker exec grs2 grs /home/L1C --cams_file /home/CAMS -o $4 --resolution $6
+else
+    sudo docker exec grs2 grs /home/L1C --surfwater $7 --cams_file /home/CAMS -o $4 --resolution $6
+fi
 sudo docker cp grs2:/home/$4 $5/$4
 echo Result written at location $5/$4
 
