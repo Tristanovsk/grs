e181765bf744fada5710edfceb519b1d79603f2b
harmelt <tristan.harmel@get.omp.eu>
2024-02-12 18:32:15 +0100

    clean up

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-# import ez_setup
-# ez_setup.use_setuptools()
-
 from setuptools import setup, find_packages
-
 
 __package__ = 'grs'
 __version__ = '2.1.3'
@@ -11,13 +7,11 @@ setup(
     name=__package__,
     version=__version__,
     packages=find_packages(),
-    #package_dir={"data": "data"},
-    package_data={'': ['*.so', '*h', '*angles*'],
-    #     # If any package contains *.txt files, include them:
-         '': ['data/aux/*.txt'],
-         'grs':['*.yml'],
-         '':['data/lut/gases/*.nc'],
-    #     'aux': ['data/aux/*']
+    package_data={
+        # If any package contains *.txt files, include them:
+        'grs': ['*.yml'],
+        '': ['data/lut/gases/*.nc', 'data/aux/*.txt'],
+
     },
     include_package_data=True,
 
@@ -29,11 +23,7 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=['cdsapi', 'pandas', 'geopandas', 'scipy', 'numba', 'numpy', 'netCDF4',
-                      'matplotlib', 'docopt',  'python-dateutil', 'pyproj',
-                       'xarray', 'dask', 'dask[array]'],
-
-    # install_requires=['cdsapi','pandas==1.1.3','geopandas==0.9.0','scipy==1.5.3','numpy==1.19.1','netCDF4==1.5.5',
-    #                  'matplotlib==3.3.3','docopt==0.6.2','GDAL==3.0.2','python-dateutil==2.8.1','pyproj==3.0.0.post1', 'Shapely==1.7.1', 'xarray'],
+                      'matplotlib', 'docopt', 'python-dateutil', 'pyproj', 'xarray'],
 
     entry_points={
         'console_scripts': [
