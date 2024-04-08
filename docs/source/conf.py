696fd6e92380260c530f9fdbeca53af900e68c09
harmelt <tristan.harmel@get.omp.eu>
2024-04-08 16:21:58 +0200

    add documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
-    'myst_nb',
+    #'myst_nb',
     'IPython.sphinxext.ipython_console_highlighting'
 
 ]
